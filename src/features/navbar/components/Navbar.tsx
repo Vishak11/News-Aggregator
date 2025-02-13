c9f9566253d34f7f9949cca2f9a605aaa376d472
Vishak11 <vishakvn007@gmail.com>
2025-02-13 12:28:59 +0530

    new styles for nav bar

--- src/features/navbar/components/Navbar.tsx
+++ src/features/navbar/components/Navbar.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import '../styles/Navbar.css'
+import React, { useState } from "react";
+import '../styles/Navbar.css';
 import SearchBar from "../../search/components/Searchbar";
 
 interface NavItem {
   label: string;
   href: string;
+  isExternal?: boolean;
   isLive?: boolean;
 }
 
 const Navbar: React.FC = () => {
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const primaryNavItems: NavItem[] = [
     { label: "Home", href: "/" },
@@ -18,9 +19,12 @@ const Navbar: React.FC = () => {
     { label: "Politics", href: "/politics" },
     { label: "Entertainment", href: "/entertainment" },
     { label: "Technology", href: "/technology" },
+    { label: "Watch Live", href: "https://www.youtube.com/watch?v=YDfiTGGPYCk" ,isExternal: true}
   ];
 
- 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="navbar">
@@ -28,23 +32,32 @@ const Navbar: React.FC = () => {
         <div className="nav-main">
           <div className="logo">ABC NEWS</div>
           
-          <div className="nav-links">
-          <SearchBar/>
+          <button className="hamburger" onClick={toggleMenu}>
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+          </button>
+
+          <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+           
+            <div className="search-container">
+              <SearchBar/>
+            </div>
             {primaryNavItems.map((item) => (
               <a
-                key={item.label}
-                href={item.href}
-                className={`nav-link ${item.isLive ? "live" : ""}`}
-              >
-                {item.isLive && <span className="live-dot"></span>}
-                {item.label}
-              </a>
+              key={item.label}
+              href={item.href}
+              className={`nav-link ${item.isLive ? "live" : ""}`}
+              target={item.isExternal ? "_blank" :""}
+              rel={item.isExternal ? "noopener noreferrer":""}
+            >
+              {item.isLive && <span className="live-dot"></span>}
+              {item.label}
+            </a>
             ))}
           </div>
         </div>
       </div>
-
-      
     </nav>
   );
 };
